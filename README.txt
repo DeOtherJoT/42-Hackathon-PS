e99e126cecb01470acae37910eaa9cd3f9100522
JoshT <wunjosh18@gmail.com>
2022-06-27 20:37:34 +0800

    Added README and some explanations

---
+++ README.txt
@@ -0,0 +1,5 @@
+Current issues with the code :
+
+1. Getting product_quantity is not perfect. It sometimes work and most of the time doesn't.
+2. In the case of having two options such as 64 GB / 128 GB, the program does not update, but a potential fix is the class tag updates to class="xxxx-selected"
+3. Other potential issues I overlooked.
--- test_scrape.py
+++ test_scrape.py
@@ -1,27 +1,33 @@
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 import requests
 
+# the link to the Lazada website.
 url = "https://www.lazada.com.my/products/redmi-note-11-11s10s6gb8gb128gboriginal-xiaomi-malaysia-ready-stock-i2804862797.html"
 
+# sends a request to get the html file using the link
 data = requests.get(url).text
+
+# makes the html file readable
 soup = BeautifulSoup(data)
 
+# looks through the html file for <h1 class="pdp-mod-product-badge-title"> and the .text takes the text within the tags
 product_name = soup.find('h1', class_ = 'pdp-mod-product-badge-title').text
-product_price = soup.find('span', class_ = 'pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl').text
-#temp = soup.find('span', class_ = 'quantity-content-default').text
 
+# same as above, but gets the price
+product_price = soup.find('span', class_ = 'pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl').text
+
+# my attempt to try and get the stock data of the product. Not perfect yet :(
+# tells the program to try to look for the tag class="quantity-content-default", but in the case of 'Out of stock', this will be an error
 try:
 	temp = soup.find('span', class_ = 'quantity-content-default').text
+# so in the case of an error {in python its called an exception, hence the keyword <except:>} it'll look for the 'Out of stock' text.
 except:
 	product_quantity = soup.find('span', class_ = "quality-content-warning").text
+# in the case where there is a stock count, it will pass the proper message to product_quantity, i.e. 'Only x items left'
 else:
 	product_quantity = soup.find('span', class_ = 'quantity-content-default').text
 
-"""if temp == None:
-	product_quantity = soup.find('span', class_ = "quantity-content-warning").text
-else:
-	product_quantity = temp.text"""
-
+# prints out the name, quantity and price of the product
 print(product_name)
 print(product_quantity)
 print(product_price)
